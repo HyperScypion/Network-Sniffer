2c40a239b2b609ab8343878aa47ab8247e8851bd
hyperscypion <tomasz_derek@wp.pl>
2018-12-28 22:47:44 +0100

    IP and UDP and TCP

--- sniffer/functions.c
+++ sniffer/functions.c
@@ -13,6 +13,8 @@
 #include <linux/if_packet.h>
 #include <net/ethernet.h>
 #include <linux/if_ether.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
 #include "functions.h"
 
 void print_hex(unsigned char *adr, unsigned char size) {
@@ -53,7 +55,7 @@ void print_dhcp(struct sockaddr_ll *sockll) {
 	printf("\t\tInterface number: %u\n", sockll->sll_ifindex);
 	printf("\t\tRP hardware type: %d\n", sockll->sll_hatype);
 	printf("\t\tPacket type %x\n", sockll->sll_pkttype);
-	printf("\t\tLength of address %d\n", sockll->sll_halen);	
+	printf("\t\tLength of address %d", sockll->sll_halen);
 }
 
 void print_eth(struct ethhdr *eth) {
@@ -62,11 +64,11 @@ void print_eth(struct ethhdr *eth) {
 	print_hex((void *)&eth->h_dest, ETH_ALEN);
 	printf("Source ehernet address: ");
 	print_hex((void *)&eth->h_source, ETH_ALEN);
-	printf("Packet type ID: 0x%04x\n", ntohs(eth->h_proto));
+	printf("Packet type ID: 0x%04x", ntohs(eth->h_proto));
 }
 
 void print_ip4(struct iphdr *ip) {
-	printf("\t~~~IP~~~\n");
+	printf("\n\t~~~IP~~~\n");
 	printf("\tVersion: %u\n", ip->version);
 	printf("\tToS: %u\n", ip->tos);
 	printf("\tTotal length: %u\n", ip->tot_len);
@@ -79,14 +81,41 @@ void print_ip4(struct iphdr *ip) {
 	print_dec((void *)ip+12, 4);
 	printf("\tDestination address: ");
 	print_dec((void *)ip+16, 4);
-	printf("\n");
+	switch (ip->protocol) {
+		case IPPROTO_TCP:
+			print_tcp((void *)ip+ip->ihl*4);
+			break;
+		case IPPROTO_UDP:
+			print_udp((void *)ip+ip->ihl*4);
+			break;
+	}
 }
 
 void print_ip6(struct ip6_hdr *ip6) {
     printf("\t~~~IPV6~~~\n");
     printf("\tPayload length: %u\n", ntohs(ip6->ip6_plen));
     printf("\tNext header: %u\n", ntohs(ip6->ip6_nxt));
-    printf("\tHop limit: %u\n", ntohs(ip6->ip6_hlim));
+    printf("\tHop limit: %u", ntohs(ip6->ip6_hlim));
 
 }
 
+void print_udp(struct udphdr *udp) {
+	printf("\t\t~~~UDP~~~\n");
+	printf("\t\tSource: %u\n", ntohs(udp->source));
+	printf("\t\tDestination: %u\n", ntohs(udp->dest));
+	printf("\t\tLength: %u\n", ntohs(udp->len));
+	printf("\t\tCheck: %u", udp->source);
+}
+
+void print_tcp(struct tcphdr *tcp) {
+	printf("\t\t~~~TCP~~~\n");
+	printf("\t\tSource port: %u\n", ntohs(tcp->source));
+	printf("\t\tDestination port: %u\n", ntohs(tcp->dest));
+	printf("\t\tSequence: %u\n", ntohl(tcp->seq));
+	printf("\t\tAck sequence: %u\n", ntohl(tcp->ack_seq));
+	printf("\t\tWindow: %u\n", ntohs(tcp->window));
+	printf("\t\tCheck: %u\n", tcp->check);
+	printf("\t\tUrgent pointer: %u\n", ntohs(tcp->urg_ptr));
+
+
+}
--- sniffer/functions.h
+++ sniffer/functions.h
@@ -4,6 +4,8 @@
 #include <netinet/ip.h>
 #include <netinet/ip6.h>
 #include <net/if.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
 #define MAXBUF 65536
 void print_arp(struct arphdr *arp);
 void print_hex(unsigned char *adr, unsigned char size);
@@ -12,4 +14,6 @@ void print_dhcp(struct sockaddr_ll *sockll);
 void print_eth(struct ethhdr *eth);
 void print_ip4(struct iphdr *ip);
 void print_ip6(struct ip6_hdr *ip6);
+void print_tcp(struct tcphdr *tcp);
+void print_udp(struct udphdr *udp);
 #endif
